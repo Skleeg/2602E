8bfdb216f351d412e5a39193d4b64a8348250efe
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2018-02-16 16:44:08 -0500

    uh
    started work on it

--- States/TipperTestMulti.c
+++ States/TipperTestMulti.c
@@ -14,90 +14,56 @@
 
 void liftControl(int leftspeed, int rightspeed)
 {
-leftspeed = motor[leftMobileGoal];
-rightspeed = motor[rightMobileGoal];
+motor[leftMobileGoal] = leftspeed;
+motor[rightMobileGoal] = rightspeed;
 }
 
 void tipControl(int leftTip, int rightTip)
 {
-	leftTip = motor[leftTipper];
-	rightTip = motor[rightTipper];
+	 motor[leftTipper] = leftTip;
+	motor[rightTipper] = rightTip;
 }
 
 		task MogoUp
 		{
-			stopTask(MogoUp);
+
 			liftControl(-127, -127);
 		}
 
 		task MogoDown
 		{
-		stopTask(MogoDown);
+
 		liftControl(127, 127);
 		}
 
 		task tipUp
 		{
-			stopTask(TipUp);
 			tipControl(-63, -63);
 		}
 
 		task tipDown
 		{
-			stopTask(tipUp);
 			tipControl(63, 63);
+			wait1Msec(450);
 		}
-
-
 
 task main()
 {
-	bLCDBacklight = true;
-	string mainBattery, backupBattery;
-
-	while(1==1)
+	while (1 == 1)
 	{
-		clearLCDLine(0);
-		clearLCDLine(1);
-		//Primary battery voltage display
-		displayLCDString(0, 0, "Primary: ");
-		sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0,'V');
-		displayNextLCDString(mainBattery);
-		//Backup battery voltage display
-		displayLCDString(1, 0, "Backup: ");
-		sprintf(backupBattery, "%1.2f%c", BackupBatteryLevel/1000.0, 'V');
-		displayNextLCDString(backupBattery);
-
-		if(vexRT[Btn6U] == 1)
-		{
-			startTask(MogoUp);
-
-		}
-		else if(vexRT[Btn6D] == 1)
-		{
-			motor[rightMobileGoal] = 127;
-			motor[leftMobileGoal] = 127;
-		}
-		else
-		{
-			motor[rightMobileGoal] = 0;
-			motor[leftMobileGoal] = 0;
-		}
 		if(vexRT[Btn5U] == 1)
 		{
-			motor[rightTipper] = -63;
-			motor[leftTipper] = -63;
-		}
-		else if(vexRT[Btn5D] == 1)
-		{
-			motor[rightTipper] = 63;
-			motor[leftTipper] = 63;
-		}
-		else
-		{
-			motor[rightTipper] = 0;
-			motor[leftTipper] = 0;
-		}
-
+			startTask(tipdown);
+			startTask(tipUp);
+	}
+	else if(vexRT[Btn5D] == 1)
+	{
+		stopTask(tipUp);
+		startTask(tipDown);
+	}
+	else
+	{
+		tipControl(0, 0);
+	}
 }
 }
