00fd7731ad8f9820864363a461643cfd0f505260
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2018-02-26 15:09:25 -0500

    pid shit
    ye

--- Drive Programs/simpletylerdrive.c
+++ Drive Programs/simpletylerdrive.c
@@ -6,7 +6,7 @@
 #pragma config(Motor,  port3,           rightMobileGoal, tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port4,           mainLift,      tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port5,           turntableMotor, tmotorVex393_MC29, openLoop)
-#pragma config(Motor,  port6,           leftSideDrive, tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port6,           leftSideDrive, tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port7,           gripMotor,     tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port8,           liftExtension, tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port9,           leftMobileGoal, tmotorVex393_MC29, openLoop)
